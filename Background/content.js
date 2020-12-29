62daa7e6ed17d1ccf37bdb9e20a1b43597a1153a
UtpalTheDev <utpalpati8@gmail.com>
2020-12-29 22:05:02 +0530

    initial-commit

---
+++ Background/content.js
@@ -0,0 +1,8 @@
+
+document.body.style.backgroundColor="black";
+var a=document.querySelector("div");
+a.style.backgroundColor="gray";
+var b=document.querySelector("section");
+var c=document.querySelector("main");
+b.style.backgroundColor="gray";
+c.style.backgroundColor="gray";
