825a42bcb2b37a93b77a256a787775e09281ea8d
UtpalTheDev <utpalpati8@gmail.com>
2020-12-30 23:45:43 +0530

    initial commit

---
+++ Addblocker/background.js
@@ -0,0 +1,17 @@
+//URLs to redirect
+var pattern = "*://securepubads.g.doubleclick.net/*";
+
+// cancel function returns an object
+// which contains a property `cancel` set to `true`
+function cancel(requestDetails) {
+  console.log("Canceling: " + requestDetails.url);
+  return {cancel: true};
+}
+
+// add the listener,
+// passing the filter argument and "blocking"
+browser.webRequest.onBeforeRequest.addListener(
+  cancel,
+  {urls: [pattern]},
+  ["blocking"]
+);
---
+++ Addblocker/content.js
@@ -0,0 +1,1 @@
+console.log("working");// match pattern for the 
